c7fd10da0c87f85ec9bc01d95b3b9bf76f4f7d3c
mltrev23 <trevor.dev23@gmail.com>
2024-04-16 20:18:44 -0500

    get loss between examples

--- inference-with-sentence-transformer.py
+++ inference-with-sentence-transformer.py
@@ -1,6 +1,10 @@
 from sentence_transformers import SentenceTransformer
+import torch
 sentences = ["This is an example sentence", "Each sentence is converted"]
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 embeddings = model.encode(sentences)
 print(embeddings)
+
+loss_fn = torch.nn.CrossEntropyLoss()
+print(loss_fn(torch.from_numpy(embeddings[0]), torch.from_numpy(embeddings[1])))
